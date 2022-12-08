9f1c6660c58671cbf1de3a2ac17d336c43ca051c
Abdullahi-El <91474775+Abdullahi-El@users.noreply.github.com>
2022-12-08 13:50:01 +0100

    Add files via upload

---
+++ desktopapp/desktopapp.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+window=tk.Tk()
+window.title(" Abdullahi's Desktop app")
+window.geometry("600x400")
+newlabel = tk.Label(text = "visit Abdullahi's github to see more projects")
+newlabel.grid(column=0, row=0)
+window.mainloop()
